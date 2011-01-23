fd41b5211ed5589f28cfe6c83f3f2a34aad9d785
白川 博之 <shrkw@helvetica.local>
2011-01-23 22:57:47 +0900

    version up: 0.0.1 -> 0.0.2

--- init.rb
+++ init.rb
@@ -4,7 +4,9 @@ Redmine::Plugin.register :redmine_user_import do
   name 'Redmine User Import plugin'
   author 'Hiroyuki SHIRAKAWA'
   description 'User import from csv'
-  version '0.0.1'
+  version '0.0.2'
+  url 'https://github.com/shrkw/redmine_user_import'
+  author_url 'http://twitter.com/#!/shrkwh, http://d.hatena.ne.jp/shrkw/'
 
 #  permission :import_user_csv, :user_import => :index
   # caption localization does not work.
